e1b56a1b8abc6039144606d65bc2ead9166c9287
HOME <105148083+HOME400@users.noreply.github.com>
2022-05-08 22:04:59 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+import { Hill } from "./hill.js";
+import { SheepController } from "./sheepcontroller.js";
+import { Sun } from "./sun.js";
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    this.sun = new Sun();
+
+    this.hills = [
+      new Hill("#fd6bea", 0.2, 12),
+      new Hill("#ff59c2", 0.5, 8),
+      new Hill("#ff4674", 1.4, 6),
+    ];
+
+    this.sheepController = new SheepController();
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.sheepController.resize(this.stageWidth, this.stageHeight);
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    this.sun.resize(this.stageWidth, this.stageHeight);
+
+    for (let i = 0; i < this.hills.length; i++) {
+      this.hills[i].resize(this.stageWidth, this.stageHeight);
+    }
+  }
+
+  animate(t) {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    this.sun.draw(this.ctx, t);
+
+    let dots;
+    for (let i = 0; i < this.hills.length; i++) {
+      dots = this.hills[i].draw(this.ctx);
+    }
+    this.sheepController.draw(this.ctx, t, dots);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
